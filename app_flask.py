c61c048bbf456d82dbf02591679026ff0b25a40c
bryanborchal <bryanborchal597@gmail.com>
2025-10-08 09:21:11 -0300

    Add files via upload

--- app_flask.py
+++ app_flask.py
@@ -9,5 +9,37 @@ def index():
 
 ## Continue o código aqui.
 
+
+@app.route("/soma")
+def soma():
+    valor1 = float(request.args.get('valor1'))
+    valor2 = float(request.args.get('valor2'))
+    return {"resultado":valor1+valor2}
+
+
+@app.route("/subtrair")
+def subtracao():
+    valor1 = float(request.args.get('valor1'))
+    valor2 = float(request.args.get('valor2'))
+    return {"resultado":valor1-valor2}
+
+
+@app.route("/multiplicar")
+def multiplicacao():
+    valor1 = float(request.args.get('valor1'))
+    valor2 = float(request.args.get('valor2'))
+    return {"resultado":valor1*valor2}
+
+
+@app.route("/dividir")
+def divisao():
+    valor1 = float(request.args.get('valor1'))
+    valor2 = float(request.args.get('valor2'))
+    if valor2 == 0:
+        return {"erro": "Divisão por zero não é permitida"}
+
+    return {"resultado":valor1/valor2}
+
+
 if __name__ == "__main__":
     app.run(debug=True)
